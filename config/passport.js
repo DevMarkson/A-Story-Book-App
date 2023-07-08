52ff2494cdd30dd7c4a17f946937fdd80aaa5768
Markson17 <marksonfrosh@gmail.com>
2023-07-09 00:38:36 +0100

    fixed error 400: redirect_uri_mismatch

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ module.exports = function(passport) {
   passport.use(new googleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      allbackURL: 'https://story-book-app-markson.onrender.com/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       const userData = {
